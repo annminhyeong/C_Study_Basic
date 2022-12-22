7743b8f6ea7ea4577986719df6fb4ab4e3a62b51
minh <rnrnck78@naver.com>
2022-12-22 14:06:34 +0900

    struct complete

--- Project14_Solution/Project25/Project.c
+++ Project14_Solution/Project25/Project.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 
 
+int temp(int a)
+{
+	return 0;
+}
+
+
+int (*g(int a))(int)
+{
+	return temp;
+}
+
+
 int main()
 {
+	//복잡한 선언 해석하는 요령
+	//1. 이름을 찾는다
+	//2. 안쪽에서 부터 바깥쪽으로 읽기
+	//3. []와 ()가 *()보다 우선순위가 높다
+
+	int* ap[10];
+
+	typedef int* pint;
+	pint ap2[10];
+
+	float* fp(float);
+	
+	typedef float* pfloat;
+	pfloat fp2(float);
+
+	void(*pf)(int);
+
+	int* (*x[10])(void);
+
+
+	int(*f(int))[];
+
+	int* (*x2[10])(int);
 
 	return 0;
 }
---
+++ Project14_Solution/Project26/Project.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* first, const void* second)
+{
+	if (*(float*)first > *(float*)second)
+		return 1;
+	else if (*(float*)first < *(float*)second)
+		return -1;
+	else
+		return 0;
+}
+
+int main()
+{	
+	float arr[] = { 8.1f, 2.4f, 5.6f, 3.2f, 6.1f, 11.0f };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	qsort(arr, n, sizeof(float), compare);
+
+	for (int i = 0; i < n; i++)
+		printf("%f ", arr[i]);
+
+	return 0;
+}
